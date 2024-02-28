7a70640c5bbde956a3f9ad3f8c2f1cfc94412e70
Anas <anasnafei@gmail.com>
2024-02-28 11:02:49 +0300

    first commit

---
+++ tests/API/RateControllerTestTrait.php
@@ -0,0 +1,231 @@
+<?php
+
+/*
+ * This file is part of the Kimai time-tracking app.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Tests\API;
+
+use App\Entity\ActivityRate;
+use App\Entity\CustomerRate;
+use App\Entity\ProjectRate;
+use App\Entity\RateInterface;
+use App\Entity\User;
+
+/**
+ * @group integration
+ */
+trait RateControllerTestTrait
+{
+    /**
+     * @param string|int $id
+     * @param string|int|null $rateId
+     * @return string
+     */
+    abstract protected function getRateUrl($id = '1', $rateId = null): string;
+
+    abstract protected function getRateUrlByRate(RateInterface $rate, bool $isCollection): string;
+
+    /**
+     * @param string|int $id
+     * @return RateInterface[]
+     */
+    abstract protected function importTestRates($id): array;
+
+    public function testAddRateMissingEntityAction(): void
+    {
+        $data = [
+            'user' => 1,
+            'rate' => 12.34,
+            'internal_rate' => 6.66,
+            'is_fixed' => false
+        ];
+        $client = $this->getClientForAuthenticatedUser(User::ROLE_ADMIN);
+        $this->assertEntityNotFoundForPost($client, $this->getRateUrl(99), $data);
+    }
+
+    public function testAddRateMissingUserAction(): void
+    {
+        $client = $this->getClientForAuthenticatedUser(User::ROLE_ADMIN);
+        $data = [
+            'user' => 33,
+            'rate' => 12.34,
+            'internal_rate' => 6.66,
+            'is_fixed' => false
+        ];
+        $this->request($client, $this->getRateUrl(), 'POST', [], json_encode($data));
+
+        $response = $client->getResponse();
+        $this->assertEquals(400, $response->getStatusCode());
+        $this->assertApiCallValidationError($response, ['user']);
+    }
+
+    public function testAddRateActionWithInvalidUser(): void
+    {
+        $client = $this->getClientForAuthenticatedUser(User::ROLE_USER);
+        $data = [
+            'user' => null,
+            'rate' => 12.34,
+            'internal_rate' => 6.66,
+            'is_fixed' => false
+        ];
+        $this->request($client, $this->getRateUrl(), 'POST', [], json_encode($data));
+        $response = $client->getResponse();
+        $this->assertApiResponseAccessDenied($response, 'Access denied.');
+    }
+
+    public function testAddRateAction(): void
+    {
+        $client = $this->getClientForAuthenticatedUser(User::ROLE_ADMIN);
+        $data = [
+            'user' => null,
+            'rate' => 12.34,
+            'internal_rate' => 6.66,
+            'is_fixed' => false
+        ];
+        $this->request($client, $this->getRateUrl(), 'POST', [], json_encode($data));
+        $this->assertTrue($client->getResponse()->isSuccessful());
+
+        $result = json_decode($client->getResponse()->getContent(), true);
+        $this->assertIsArray($result);
+        $this->assertRateStructure($result, null);
+        $this->assertNotEmpty($result['id']);
+        $this->assertEquals(12.34, $result['rate']);
+        $this->assertEquals(6.66, $result['internalRate']);
+        $this->assertFalse($result['isFixed']);
+    }
+
+    public function testAddFixedRateAction(): void
+    {
+        $client = $this->getClientForAuthenticatedUser(User::ROLE_ADMIN);
+        $data = [
+            'user' => 1,
+            'rate' => 12.34,
+            'internal_rate' => 6.66,
+            'is_fixed' => true
+        ];
+        $this->request($client, $this->getRateUrl(), 'POST', [], json_encode($data));
+        $this->assertTrue($client->getResponse()->isSuccessful());
+
+        $result = json_decode($client->getResponse()->getContent(), true);
+        $this->assertIsArray($result);
+        $this->assertRateStructure($result, 1);
+        $this->assertNotEmpty($result['id']);
+        $this->assertEquals(12.34, $result['rate']);
+        $this->assertEquals(6.66, $result['internalRate']);
+        $this->assertTrue($result['isFixed']);
+    }
+
+    public function testGetRatesEmptyResult(): void
+    {
+        $client = $this->getClientForAuthenticatedUser(User::ROLE_ADMIN);
+        $this->request($client, $this->getRateUrl(1));
+        $this->assertTrue($client->getResponse()->isSuccessful());
+
+        $result = json_decode($client->getResponse()->getContent(), true);
+        $this->assertIsArray($result);
+        $this->assertEmpty($result);
+    }
+
+    public function testGetRates(): void
+    {
+        $client = $this->getClientForAuthenticatedUser(User::ROLE_ADMIN);
+        $expectedRates = $this->importTestRates(1);
+
+        $this->request($client, $this->getRateUrl(1));
+        $this->assertTrue($client->getResponse()->isSuccessful());
+
+        $result = json_decode($client->getResponse()->getContent(), true);
+        $this->assertIsArray($result);
+        $this->assertNotEmpty($result);
+        $this->assertEquals(\count($expectedRates), \count($result));
+
+        foreach ($result as $rate) {
+            $this->assertRateStructure($rate, ($rate['user'] === null ? null : $rate['user']['id']));
+        }
+    }
+
+    public function testGetRatesEntityNotFound(): void
+    {
+        $this->assertEntityNotFound(User::ROLE_ADMIN, $this->getRateUrl(99));
+    }
+
+    public function testGetRatesIsSecured(): void
+    {
+        $this->assertUrlIsSecuredForRole(User::ROLE_USER, $this->getRateUrl(1));
+    }
+
+    public function testDeleteRate(): void
+    {
+        $client = $this->getClientForAuthenticatedUser(User::ROLE_ADMIN);
+        $expectedRates = $this->importTestRates(1);
+
+        $this->request($client, $this->getRateUrlByRate($expectedRates[0], false), 'DELETE');
+        $this->assertTrue($client->getResponse()->isSuccessful());
+        $this->assertEmpty($client->getResponse()->getContent());
+
+        // fetch rates to validate that one was removed
+        $this->request($client, $this->getRateUrlByRate($expectedRates[0], true));
+        $this->assertTrue($client->getResponse()->isSuccessful());
+
+        $result = json_decode($client->getResponse()->getContent(), true);
+        $this->assertIsArray($result);
+        $this->assertEquals(\count($expectedRates) - 1, \count($result));
+    }
+
+    public function testDeleteRateEntityNotFound(): void
+    {
+        $client = $this->getClientForAuthenticatedUser(User::ROLE_ADMIN);
+        $this->assertNotFoundForDelete($client, $this->getRateUrl(99, 1));
+    }
+
+    public function testDeleteRateRateNotFound(): void
+    {
+        $client = $this->getClientForAuthenticatedUser(User::ROLE_ADMIN);
+        $this->assertNotFoundForDelete($client, $this->getRateUrl(1, 99));
+    }
+
+    public function testDeleteRateWithInvalidAssignment(): void
+    {
+        $client = $this->getClientForAuthenticatedUser(User::ROLE_ADMIN);
+        $this->importTestRates(1);
+        $this->importTestRates(2);
+
+        $this->assertNotFoundForDelete($client, $this->getRateUrl(2, 1));
+    }
+
+    public function testDeleteNotAllowed(): void
+    {
+        $client = $this->getClientForAuthenticatedUser(User::ROLE_USER);
+        $rates = $this->importTestRates(1);
+
+        /** @var ActivityRate|ProjectRate|CustomerRate $rate */
+        $rate = $rates[0];
+
+        $this->request($client, $this->getRateUrl(1, $rate->getId()), 'DELETE');
+        $this->assertApiResponseAccessDenied($client->getResponse(), 'Access denied.');
+    }
+
+    public function assertRateStructure(array $result, $user = null): void
+    {
+        $expectedKeys = [
+            'id', 'rate', 'internalRate', 'isFixed', 'user'
+        ];
+
+        $actual = array_keys($result);
+        sort($actual);
+        sort($expectedKeys);
+
+        $this->assertEquals($expectedKeys, $actual, 'Rate structure does not match');
+
+        if (null !== $user) {
+            self::assertIsArray($result['user'], 'Rate user is not an array');
+            self::assertEquals($user, $result['user']['id'], 'Rate user does not match');
+        } else {
+            self::assertNull($result['user']);
+        }
+    }
+}
---
+++ tests/Repository/Query/ExportQueryTest.php
@@ -0,0 +1,58 @@
+<?php
+
+/*
+ * This file is part of the Kimai time-tracking app.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Tests\Repository\Query;
+
+use App\Repository\Query\ExportQuery;
+
+/**
+ * @covers \App\Repository\Query\ExportQuery
+ * @covers \App\Repository\Query\TimesheetQuery
+ */
+class ExportQueryTest extends TimesheetQueryTest
+{
+    public function testQuery(): void
+    {
+        $sut = new ExportQuery();
+
+        $this->assertPage($sut);
+        $this->assertPageSize($sut);
+        $this->assertOrderBy($sut, 'begin');
+        $this->assertOrder($sut, ExportQuery::ORDER_ASC);
+
+        $this->assertUser($sut);
+        $this->assertCustomer($sut);
+        $this->assertProject($sut);
+        $this->assertActivity($sut);
+        $this->assertStateWith($sut, ExportQuery::STATE_STOPPED);
+        $this->assertExportedWith($sut, ExportQuery::STATE_NOT_EXPORTED);
+        $this->assertRenderer($sut);
+        $this->assertMarkAsExported($sut);
+    }
+
+    public function assertMarkAsExported(ExportQuery $sut): void
+    {
+        $this->assertTrue($sut->isMarkAsExported());
+
+        $sut->setMarkAsExported(false);
+        $this->assertFalse($sut->isMarkAsExported());
+    }
+
+    public function assertRenderer(ExportQuery $sut): void
+    {
+        $this->assertNull($sut->getRenderer());
+
+        $exportTypes = ['html', 'csv', 'pdf', 'xlsx', 'ods'];
+
+        foreach ($exportTypes as $type) {
+            $sut->setRenderer($type);
+            $this->assertEquals($type, $sut->getRenderer());
+        }
+    }
+}
